5c42181746204434f63215eede6c22565924ba09
TaimurHasan <taimurhasan11@gmail.com>
2022-09-07 14:48:54 -0400

    server successfully running with test typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,16 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        events: Int
+    }
+    type Query {
+        users: [User]
+        user(username: String!): User
+    }
+`;
 
 module.exports = typeDefs;
