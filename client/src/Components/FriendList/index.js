6a052446416765ffc408c504abc89fb6bd5b14bb
TaimurHasan <taimurhasan11@gmail.com>
2022-09-13 20:17:53 -0400

    rendered added friend on frontend

--- client/src/Components/FriendList/index.js
+++ client/src/Components/FriendList/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import classes from './FriendList.module.css';
+import { AiOutlineUsergroupAdd } from 'react-icons/ai';
 
 const FriendList = ({ friends }) => {
 
     return (
         <div className="container my-4">
-            <h3 className="mx-1">Your Friends:</h3>
+            <div className="container d-flex justify-content-between">
+                <h3 className="mx-1">Your Friends:</h3>
+                <AiOutlineUsergroupAdd className = {classes.addFriendBtn} size = "35px"/>
+            </div>
             {friends.length ? (
                 <div className="mx-1">
                     {friends.map(friend => (
-                        <a href='/' key={friend._id}>
-                            <div className={`my-2 ${classes.friendCard}`}>
+                        <a className={classes.friendLink} href='/' key={friend._id}>
+                            <div className={`${classes.friendCard}`}>
                                 <div>
                                     <h4>{friend.username}</h4>
                                 </div>
