36b9683c88eb0a7123381aa0f29523c6b5ad9dc3
TaimurHasan <taimurhasan11@gmail.com>
2022-09-24 14:40:35 -0400

    added new image and additional centering to landing page

--- client/src/Components/BenefitsList/index.js
+++ client/src/Components/BenefitsList/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import createEvent from "../../assets/images/firedupcreateevent.png";
+import addFriends from "../../assets/images/firedupaddfriends.png";
 import classes from "./BenefitsList.module.css";
 
 const BenefitsList = () => {
@@ -7,8 +8,8 @@ const BenefitsList = () => {
     const benefits = [
         {   
             key: 1,
-            name: "Add Friends",
-            image: createEvent,
+            name: "View Events & Add Friends",
+            image: addFriends,
             alt: "create event page of firedup",
             description: "Invite and find your friends on your dashboard to see what they're up to.",
             float: "left"
@@ -25,20 +26,20 @@ const BenefitsList = () => {
 
     return (
         <div className={`${classes.benefitsDiv}`}>    
-            <div className="container d-flex flex-column align-items-start flex-wrap py-4 px-0">
+            <div className="container d-flex flex-column flex-wrap py-4 px-0">
                 {benefits.map(benefit => (
                     benefit.float === "left" ? (
-                            <div key={benefit.key} className="d-flex my-4 py-4">
-                                <div className={classes.image}>
-                                    <img src={benefit.image} alt={benefit.alt}/>
-                                </div>
-                                <div className={classes.benefitsDetails}>
-                                    <h3>{benefit.name}</h3>
-                                    <p>{benefit.description}</p>
-                                </div>
+                            <div key={benefit.key} className={`d-flex my-4 py-4 ${classes.benefitCard}`}>
+                                    <div className={classes.image}>
+                                        <img src={benefit.image} alt={benefit.alt}/>
+                                    </div>
+                                    <div className={classes.benefitsDetails}>
+                                        <h3>{benefit.name}</h3>
+                                        <p>{benefit.description}</p>
+                                    </div>
                             </div>
                         ) : (
-                            <div key={benefit.key} className="d-flex my-4 py-4">
+                            <div key={benefit.key} className={`d-flex my-4 py-4 ${classes.benefitCard}`}>
                                 <div className={classes.benefitsDetails}>
                                     <h3>{benefit.name}</h3>
                                     <p>{benefit.description}</p>
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -4,6 +4,7 @@ import FriendList from "../Components/FriendList";
 import { useQuery } from "@apollo/client";
 import { QUERY_ME } from "../utils/queries";
 import Loading from "../Components/Loading";
+import classes from "../css/Dashboard.module.css"
 
 const Dashboard = () => {
     const { loading, data } = useQuery(QUERY_ME);
@@ -21,7 +22,7 @@ const Dashboard = () => {
     return(
         <div className="container">
             <h2 className="my-3 mx-1">Welcome, {user.username}!</h2>
-            <div className="container d-flex">
+            <div className={`container d-flex ${classes.dashboardMain}`}>
                 <div className="col-lg-7">
                     <EventList events = {user.events} />
                 </div>
